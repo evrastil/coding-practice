7a61f9649db400464b643b308db6a03cdcb814f0
Eduard Vrastil <eduard.vrastil@hp.com>
2016-11-09 15:17:32 +0100

    linked list cycles

--- src/LinkedListCycleTest.java
+++ src/LinkedListCycleTest.java
@@ -11,56 +11,84 @@ import java.util.*;
  * |_________|
  */
 public class LinkedListCycleTest {
-    @Test
-    public void testCycleInList() {
-        Node node1 = new Node(1);
-        Node node2 = new Node(2);
-        Node node3 = new Node(3);
-        Node node4 = new Node(4);
-        node1.children.add(node2);
-        node2.children.add(node3);
-        node3.children.add(node1);
-        node3.children.add(node4);
-        Assert.assertTrue(isCycleInLinkedList(node1));
-    }
+//    @Test
+//    public void testCycleInList() {
+//        Node node1 = new Node(1);
+//        Node node2 = new Node(2);
+//        Node node3 = new Node(3);
+//        Node node4 = new Node(4);
+//        node1.children.add(node2);
+//        node2.children.add(node3);
+//        node3.children.add(node1);
+//        node3.children.add(node4);
+//        Assert.assertTrue(isCycleInLinkedList(node1));
+//    }
 
-    private boolean isCycleInLinkedList(Node node) {
-        Queue<Node> toVisitQueue = new LinkedList<>();
-        Map<Node, Node> visitedMap = new HashMap<>();
-        toVisitQueue.add(node);
-        while (!toVisitQueue.isEmpty()) {
-            Node temp = toVisitQueue.poll();
-            for (Node child : temp.children) {
-                if (visitedMap.containsKey(child)) {
-                    return true;
-                }
-                toVisitQueue.add(child);
-                visitedMap.put(node, node);
-            }
-        }
-        return false;
-    }
-
-
-    private class Node {
-        int value;
-
-        Node(int value) {
-            this.value = value;
-        }
-
-        private List<Node> children = new ArrayList<>();
-    }
+//    private boolean isCycleInLinkedList(Node node) {
+//        Queue<Node> toVisitQueue = new LinkedList<>();
+//        Map<Node, Node> visitedMap = new HashMap<>();
+//        toVisitQueue.add(node);
+//        while (!toVisitQueue.isEmpty()) {
+//            Node temp = toVisitQueue.poll();
+//            for (Node child : temp.children) {
+//                if (visitedMap.containsKey(child)) {
+//                    return true;
+//                }
+//                toVisitQueue.add(child);
+//                visitedMap.put(node, node);
+//            }
+//        }
+//        return false;
+//    }
+//
+//
+//    private class Node {
+//        int value;
+//
+//        Node(int value) {
+//            this.value = value;
+//        }
+//
+//        private List<Node> children = new ArrayList<>();
+//    }
 
 /**
  * 1 -> 2 -> 3 -> 4
  *      ^         |
  *      |_________|
  */
-//    private class Node{
-//        int val;
-//        Node next;
-//    }
+
+    @Test
+    public void testCycleInList() {
+        Node node1 = new Node();
+        Node node2 = new Node();
+        Node node3 = new Node();
+        Node node4 = new Node();
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node2;
+        Assert.assertTrue(isCycleInLinkedList(node1));
+    }
+
+    private boolean isCycleInLinkedList(Node node) {
+        Set<Node> visited = new HashSet<>();
+        Node temp = node;
+        while (temp.next != null){
+            if(visited.contains(temp)){
+                return true;
+            }
+            visited.add(temp);
+            temp = temp.next;
+        }
+
+        return false;
+    }
+
+    private class Node{
+        Node next;
+    }
+
 
     // Algorithm using extra space. Mark visited nodes and check that you
 // only visit each node once.
