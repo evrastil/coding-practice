192d92718334007a44daac10cec4063e9efcb82d
Eduard Vrastil <eduard.vrastil@hp.com>
2017-01-24 09:55:15 +0100

    todo

---
+++ src/FindGroupedNumbersInTwoDArrayTest.java
@@ -0,0 +1,5 @@
+/**
+ * Created by vrastil on 24.1.2017.
+ */
+public class FindGroupedNumbersInTwoDArrayTest {
+}
--- src/RestoreSpacesInSentenceTest.java
+++ src/RestoreSpacesInSentenceTest.java
@@ -1,6 +1,6 @@
 /**
  * Restore sentence from which spaces between words were removed, given dictionary of words.
  */
-public class RestoreSentenceWithoutSpacesTest {
+public class RestoreSpacesInSentenceTest {
     //TODO
 }
