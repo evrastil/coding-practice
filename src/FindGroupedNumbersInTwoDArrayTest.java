e20d7ffa47e0dc0e9ee8b26b35aff8e40a33b97f
Eduard Vrastil <eduard.vrastil@hp.com>
2017-01-24 17:18:27 +0100

    todo

--- src/FindGroupedNumbersInTwoDArrayTest.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by vrastil on 24.1.2017.
- */
-public class FindGroupedNumbersInTwoDArrayTest {
-}
---
+++ src/MarkIslandTest.java
@@ -0,0 +1,5 @@
+/**
+ * Write code which will accept this 2-dim array and return the number of islands - See more at: http://www.ritambhara.in/islands-in-a-two-dimensional-array/#sthash.FQf0kYW3.dpuf
+ */
+public class MarkIslandTest {
+}
