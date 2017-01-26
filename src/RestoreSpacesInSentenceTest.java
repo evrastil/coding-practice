bd4f898d3ea08f01d5b2eb20b9eb32574f443989
Eduard Vrastil <eduard.vrastil@hpe.com>
2017-01-26 16:52:11 +0100

    wip

--- src/RestoreSpacesInSentenceTest.java
+++ src/RestoreSpacesInSentenceTest.java
@@ -1,6 +1,42 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Restore sentence from which spaces between words were removed, given dictionary of words.
+ * <p>
+ * Consider the following dictionary
+ * { i, like, sam, sung, samsung, mobile, ice, cream, icecream, man, go, mango}
+ * <p>
+ * Input:  ilike
+ * Output: Yes
+ * The string can be segmented as "i like".
+ * <p>
+ * Input:  ilikesamsung
+ * Output: Yes
+ * The string can be segmented as "i like samsung" or "i like sam sung".
  */
 public class RestoreSpacesInSentenceTest {
-    //TODO
+
+    @Test
+    public void testRestoreSentence() {
+        String[] dictionary = new String[]{"i", "hate", "sam", "sung", "samsung", "mobile", "ice", "cream", "icecream", "man", "go", "mango" };
+        String mess = "manihatemobilesamsung";
+        restore(mess, dictionary, "");
+    }
+
+    public void restore(String mess, String[] dictionary, String result) {
+        for (int i = 1; i <= mess.length(); i++) {
+            String substring = mess.substring(0, i);
+            for (int j = 0; j < dictionary.length; j++) {
+                String word = dictionary[j];
+                if (word.equals(substring)) {
+                    result = result + " " + word;
+                    restore(mess.substring(i, mess.length()), dictionary, result);
+                }
+            }
+        }
+        System.out.println(result);
+    }
 }
