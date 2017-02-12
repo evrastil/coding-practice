244af6ff9c8d0a56c4684b4d3f5911cb787e8413
Eduard Vrastil <eduard.vrastil@hp.com>
2017-02-12 09:46:37 +0100

    codility

--- src/CodilityOddOccurrencesInArrayTest.java
+++ src/CodilityOddOccurrencesInArrayTest.java
@@ -2,9 +2,7 @@ import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 
 /**
  * Created by vrastil on 10.2.2017.
