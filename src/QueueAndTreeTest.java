f812631e3290277b6700f49a3db89d4ba13e385f
Eduard Vrastil <eduard.vrastil@hpe.com>
2017-02-28 16:33:37 +0100

    collections

---
+++ src/QueueAndTreeTest.java
@@ -0,0 +1,22 @@
+import org.junit.Test;
+
+import java.util.PriorityQueue;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+/**
+ * Created by eda on 28.2.17.
+ */
+public class QueueAndTreeTest {
+    @Test
+    public void testIt(){
+        PriorityQueue  priorityQueue = new PriorityQueue();
+        priorityQueue.peek();
+        priorityQueue.spliterator();
+        TreeMap treeMap = new TreeMap();
+        treeMap.floorEntry();
+        treeMap.pollFirstEntry()
+        TreeSet treeSet = new TreeSet();
+
+    }
+}
