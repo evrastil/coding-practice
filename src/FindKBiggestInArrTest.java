f547d56e0010e558e4160acbeaa8088583b49a11
eduard <evrastil@gmail.com>
2017-03-14 10:04:29 +0100

    max heap

--- src/FindKBiggestInArrTest.java
+++ src/FindKBiggestInArrTest.java
@@ -1,12 +1,15 @@
 import org.junit.Test;
 
+import java.util.Collections;
+import java.util.PriorityQueue;
+
 public class FindKBiggestInArrTest {
 
     @Test
     public void testIt() {
         int[] arr = new int[]{5, 400, 8, 9, 6, 3, 4, 7, 52}; //400,52,9,8,7,6,5,4,3
-        int i = findKBiggest(8, arr);
-        System.out.println(i);
+        System.out.println(findKBiggest(8, arr));
+        System.out.println(findKBiggest2(8, arr));
     }
 
     //bubble sort
@@ -27,6 +30,22 @@ public class FindKBiggestInArrTest {
             }
         }
         return arr[K - 1];
+    }
+
+    // max heap
+    public int findKBiggest2(int K, int[] arr) {
+        if (K > arr.length) {
+            return -1;
+        }
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<Integer>(arr.length, Collections.reverseOrder());
+        for (int i = 0; i < arr.length; i++) {
+            int i1 = arr[i];
+            priorityQueue.add(i1);
+        }
+        for (int i = 0; i < K - 1; i++) {
+            priorityQueue.poll();
+        }
+        return priorityQueue.poll();
     }
 
 
