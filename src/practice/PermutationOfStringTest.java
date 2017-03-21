0fee8c617a76e68a0d17aa1c772fc4e8e4f6f2ac
Eduard Vrastil <eduard.vrastil@hpe.com>
2017-03-21 10:56:39 +0100

    PermutationOfStringTest

--- src/practice/PermutationOfStringTest.java
+++ src/practice/PermutationOfStringTest.java
@@ -4,18 +4,18 @@ import org.junit.Test;
 
 public class PermutationOfStringTest {
     @Test
-    public void testIt(){
+    public void testIt() {
         String s = "abc";
         permutations("", s);
     }
 
     private void permutations(String prefix, String s) {
-        int length  = s.length();
-        if(s.length() == 0){
+        int length = s.length();
+        if (length == 0) {
             System.out.println(prefix);
         }
-        for (int i = 0; i < s.length(); i++) {
-            permutations(prefix + s.charAt(i), s.substring(0,i)+s.substring(i+1, length));
+        for (int i = 0; i < length; i++) {
+            permutations(prefix + s.charAt(i), s.substring(0, i) + s.substring(i + 1, length));
         }
     }
 }
