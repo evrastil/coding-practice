674c5528b6e5e97d28bdb372348fbafcc213f3e4
Eduard Vrastil <eduard.vrastil@hpe.com>
2017-03-21 11:01:35 +0100

    PermutationOfStringTest

--- src/practice/PermutationOfStringTest.java
+++ src/practice/PermutationOfStringTest.java
@@ -2,20 +2,29 @@ package practice;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PermutationOfStringTest {
     @Test
     public void testIt() {
         String s = "abc";
-        permutations("", s);
+        List<String> result = new ArrayList<>();
+        permutations("", s, result);
+        for (String r : result) {
+            System.out.println(r);
+        }
+
     }
 
-    private void permutations(String prefix, String s) {
+    private void permutations(String prefix, String s, List<String> result) {
         int length = s.length();
         if (length == 0) {
-            System.out.println(prefix);
+//            System.out.println(prefix);
+            result.add(prefix);
         }
         for (int i = 0; i < length; i++) {
-            permutations(prefix + s.charAt(i), s.substring(0, i) + s.substring(i + 1, length));
+            permutations(prefix + s.charAt(i), s.substring(0, i) + s.substring(i + 1, length), result);
         }
     }
 }
