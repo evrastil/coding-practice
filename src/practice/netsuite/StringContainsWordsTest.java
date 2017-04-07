73c8fae6b2ef48cbf103e30b6d9a190f4e2ac928
eduard <evrastil@gmail.com>
2017-04-07 11:36:28 +0200

    StringContainsWordsTest

--- src/practice/netsuite/StringContainsWordsTest.java
+++ src/practice/netsuite/StringContainsWordsTest.java
@@ -3,28 +3,32 @@ package practice.netsuite;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class StringContainsWordsTest {
     @Test
     public void testIt() {
-        String[] dict = new String[]{"a", "b", "c"};
-        List<String> words = new ArrayList<>();
-        containsWords("ab", dict, words);
+        String[] dict = new String[]{"a", "b", "c", "d", "e"};
+        Set<String> words = new HashSet<>();
+        containsWords("abc", dict, words);
         System.out.println(words);
     }
 
-    void containsWords(String str, String[] dict, List<String> words) {
-        if (str.length() == 0) {
+    void containsWords(String str, String[] dict, Set<String> words) {
+        int length = str.length();
+        if (length == 0) {
             return;
         }
-        for (int i = 0; i <= str.length(); i++) {
-
+        for (int i = 0; i < dict.length; i++) {
             if (dict[i].equals(str)) {
                 words.add(str);
             }
-            containsWords(str.substring(i, str.length()), dict, words);
-            containsWords(str.substring(0, str.length() - i), dict, words);
+        }
+        for (int i = 1; i < str.length(); i++) {
+            containsWords(str.substring(0, length - i), dict, words);
+            containsWords(str.substring(i, length), dict, words);
         }
     }
 }
